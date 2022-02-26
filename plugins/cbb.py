31acc04e8854a3f0f983dfe973250e8d1a802d58
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-02-26 21:54:36 +0530

    </a>

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Creator : <a href='tg://user?id={OWNER_ID}'Me</a>\n┣⪼ Language : Python3\n┣⪼ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┣⪼ Source Code : <a href='https://github.com/Aadhi000/OP-Files-Store-Bot'>OP-File Store Bot</a>\n┣⪼ Channel : <a href='https://t.me/OpusTechz'>Opus Techz</a>\n┣⪼ YouTube Channel : <a href='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA'>Opus Techz</a>\n╰━━━━━━━━━━━━━━━➣</b>",
+            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Creator : <a href='tg://user?id={OWNER_ID}'>Me</a>\n┣⪼ Language : Python3\n┣⪼ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┣⪼ Source Code : <a href='https://github.com/Aadhi000/OP-Files-Store-Bot'>OP-File Store Bot</a>\n┣⪼ Channel : <a href='https://t.me/OpusTechz'>Opus Techz</a>\n┣⪼ YouTube Channel : <a href='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA'>Opus Techz</a>\n╰━━━━━━━━━━━━━━━➣</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
