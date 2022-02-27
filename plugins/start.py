d2e18ba46f0b4590ef4aa8f4853b5d6581d89c84
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-02-27 12:46:28 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,15 +88,16 @@ async def start_command(client: Client, message: Message):
         return
     else:
         reply_markup = InlineKeyboardMarkup(
-            [                
+            [
+                    [
+                        InlineKeyboardButton("𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴", url="https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA")                        
+                    ],
+                    [
+                        InlineKeyboardButton("𝙰𝙱𝙾𝚄𝚃", callback_data = "about"),
+                        InlineKeyboardButton("𝙲𝙻𝙾𝚂𝙴", callback_data = "close")
+                    ]
                 ]
-                InlineKeyboardButton("𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴", url="https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA")
-                ],
-                [
-                InlineKeyboardButton("𝙰𝙱𝙾𝚄𝚃", callback_data = "about"),
-                InlineKeyboardButton("𝙲𝙻𝙾𝚂𝙴", callback_data = "close")
-                ]
-            ]
+            )
         )
         await message.reply_text(
             text = START_MSG.format(
