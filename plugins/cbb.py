5789eec3484817e8ff8c321485cbb7fbe26ac806
ααdhí | αrjun vr <87893573+Aadhi000@users.noreply.github.com>
2022-06-07 13:59:03 +0530

    Edit 2

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -5,15 +5,12 @@ from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-sourccc = <a href='https://github.com/Aadhi000/OP-Files-Store-Bot'>OP-File Store Bot</a>
-libr = <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>
-
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Creator : <a href='tg://user?id={OWNER_ID}'>contact</a>\n┣⪼ Language : Python3\n┣⪼ Library : {libr}\n┣⪼ Source Code : {sourccc}\n┣⪼ Movies Channel : <a href='https://t.me/+2bjgMTXq8o5kYjNl'>Tamil new movies</a>\n┣⪼ YouTube Channel : <a href='https://youtube.com/channel/UCzy4pkie1EWHwWql4CL5glg'>Chill Tamilan</a>\n╰━━━━━━━━━━━━━━━➣</b>",
+            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Creator : <a href='tg://user?id={OWNER_ID}'>contact</a>\n┣⪼ Language : Python3\n┣⪼ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┣⪼ Source Code : <a href='https://github.com/Aadhi000/OP-Files-Store-Bot'>OP-File Store Bot</a>\n┣⪼ Movies Channel : <a href='https://t.me/+2bjgMTXq8o5kYjNl'>Tamil new movies</a>\n┣⪼ YouTube Channel : <a href='https://youtube.com/channel/UCzy4pkie1EWHwWql4CL5glg'>Chill Tamilan</a>\n╰━━━━━━━━━━━━━━━➣</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
